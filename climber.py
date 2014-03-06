41ec4a58d20d6822a7e08a0dade43f8b93fcc4ec
Jeppe Toustrup <jeppe@falconsocial.com>
2014-03-06 23:43:15 +0100

    Further design work, now using a base template

--- climber.py
+++ climber.py
@@ -21,7 +21,18 @@ def index():
             'opponent_score': 10
         }
     ]
-    return render_template('index.html', recent_games=recent_games)
+    ranking = [
+        {
+            'name': 'Jeppe',
+            'games_won': 10,
+            'games_lost': 3,
+            'games_streak': 3,
+            'goals_scored': 74,
+            'goals_against': 22,
+            'points': 128
+        }
+    ]
+    return render_template('index.html', recent_games=recent_games, ranking=ranking)
 
 if __name__ == '__main__':
     app.debug = True
